ed4f86deaeb94f36dbc20d55389f89d7515d02ce
Filip Maciąg <80418239+filipmaciag@users.noreply.github.com>
2022-09-21 10:07:08 +0200

    Update Dependencies.cs
    Use only in memory db

--- src/Infrastructure/Dependencies.cs
+++ src/Infrastructure/Dependencies.cs
@@ -10,31 +10,10 @@ public static class Dependencies
 {
     public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
     {
-        var useOnlyInMemoryDatabase = false;
-        if (configuration["UseOnlyInMemoryDatabase"] != null)
-        {
-            useOnlyInMemoryDatabase = bool.Parse(configuration["UseOnlyInMemoryDatabase"]);
-        }
-
-        if (useOnlyInMemoryDatabase)
-        {
-            services.AddDbContext<CatalogContext>(c =>
-               c.UseInMemoryDatabase("Catalog"));
+          services.AddDbContext<CatalogContext>(c =>
+             c.UseInMemoryDatabase("Catalog"));
          
-            services.AddDbContext<AppIdentityDbContext>(options =>
-                options.UseInMemoryDatabase("Identity"));
-        }
-        else
-        {
-            // use real database
-            // Requires LocalDB which can be installed with SQL Server Express 2016
-            // https://www.microsoft.com/en-us/download/details.aspx?id=54284
-            services.AddDbContext<CatalogContext>(c =>
-                c.UseSqlServer(configuration.GetConnectionString("CatalogConnection")));
-
-            // Add Identity DbContext
-            services.AddDbContext<AppIdentityDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("IdentityConnection")));
-        }
+          services.AddDbContext<AppIdentityDbContext>(options =>
+             options.UseInMemoryDatabase("Identity"));
     }
 }
